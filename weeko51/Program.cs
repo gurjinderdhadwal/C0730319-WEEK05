bdeaf9b19806516d72d1d981dc1c40bdf6f4cbd1
visualstudio <visualstudio@122-PC24>
2019-02-08 20:39:01 -0500

    8.30commit

--- weeko51/Program.cs
+++ weeko51/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace weeko51
+namespace FRI_FEB_8
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Elevator e = new Elevator();
+            e.setup();
+            e.TraverseList();
         }
     }
+
     class Node
     {
         public Node() { }
-        public Node floor;
         public Node elevatorUp;
         public string FloorNumber;
     }
@@ -29,20 +27,43 @@ namespace weeko51
 
     class Elevator
     {
-//@@ -51,13 +56,12 @@ public void TraverseList()
+        Node Head;
+        Node FirstFloor;
+        Node SecondFloor;
+        Node ThirdFloor;
+        Node FourthFloor;
+
+        public void setup()
+        {
+            FirstFloor = new Node();
+            SecondFloor = new Node();
+            ThirdFloor = new Node();
+            FourthFloor = new Node();
+            Head = FirstFloor;
+            FirstFloor.FloorNumber = "First Floor";
+            FirstFloor.elevatorUp = SecondFloor;
+            SecondFloor.FloorNumber = "Second Floor";
+            SecondFloor.elevatorUp = ThirdFloor;
+            ThirdFloor.FloorNumber = "Third Floor";
+            ThirdFloor.elevatorUp = FourthFloor;
+            FourthFloor.FloorNumber = "Fourth Floor";
+            FourthFloor.elevatorUp = null;
+        }
+
+        public void TraverseList()
+        {
+            Node temp;
             temp = Head;
 
             // where am I going to start?
-            Console.WriteLine("The first floor is " + Head.FloorNumber);
 
             while (temp != null)
             {
-                // NOW GET TO THE SECOND FLOOR!!!!
-                temp = temp.elevatorUp;
                 Console.WriteLine(temp.FloorNumber);
-
                 temp = temp.elevatorUp;
 
             }
 
+        }
+    }
 }
