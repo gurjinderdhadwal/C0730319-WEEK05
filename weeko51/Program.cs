0b39d9eb2da8a0438a1725999881cf5cf461e983
visualstudio <visualstudio@122-PC24>
2019-02-08 20:35:19 -0500

    8.30

--- weeko51/Program.cs
+++ weeko51/Program.cs
@@ -10,6 +10,39 @@ namespace weeko51
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello");
         }
     }
+    class Node
+    {
+        public Node() { }
+        public Node floor;
+        public Node elevatorUp;
+        public string FloorNumber;
+    }
+
+    class Department
+    {
+        public Department aDepartment;
+
+    }
+
+    class Elevator
+    {
+//@@ -51,13 +56,12 @@ public void TraverseList()
+            temp = Head;
+
+            // where am I going to start?
+            Console.WriteLine("The first floor is " + Head.FloorNumber);
+
+            while (temp != null)
+            {
+                // NOW GET TO THE SECOND FLOOR!!!!
+                temp = temp.elevatorUp;
+                Console.WriteLine(temp.FloorNumber);
+
+                temp = temp.elevatorUp;
+
+            }
+
 }
